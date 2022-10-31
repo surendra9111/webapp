b2dde719fc32c47110b9b0d0693c31cefe7fd167
surendra9111 <108533205+surendra9111@users.noreply.github.com>
2022-10-31 22:55:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,41 @@
 # Generate a random integer to create a globally unique name
 resource "random_integer" "ri" {
   min = 10000
-  max = 99999 
+  max = 99999
 }
+
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "webapp-rg-${random_integer.ri.result}"
-  location = "west us 3"
+  name     = "myResourceGroup-${random_integer.ri.result}"
+  location = "eastus"
 }
+
 # Create the Linux App Service Plan
-resource "azurerm_app_service_plan" "appserviceplan" {
+resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Free"
-    size = "F1"
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-${random_integer.ri.result}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
   }
 }
-# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
-resource "azurerm_app_service" "pizzahut-dummy2" {
-  name                = "pizzahut-dummy2-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-  source_control {
-    repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
-  }
+
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
 }
