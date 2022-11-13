3d10ee576b035f190bff48d22a4b25f6c03cfbe4
surendra9111 <108533205+surendra9111@users.noreply.github.com>
2022-11-13 22:51:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.31.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 # Generate a random integer to create a globally unique name
 resource "random_integer" "ri" {
   min = 10000
